e2e5dccb779506af04342141faabbed7703c0d2c
drtfygh6 <js1medway10@hotmail.com>
2024-05-13 20:14:13 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-function findMaxLength(nums) {
-  const map = new Map();
-  map.set(0, -1);
-  let count = 0;
-  let maxLength = 0;
-  for (let i = 0; i < nums.length; i++) {
-    count += nums[i] === 1 ? 1 : -1;
-    if (map.has(count)) {
-      maxLength = Math.max(maxLength, i - map.get(count));
-    } else {
-      map.set(count, i);
-    }
-  }
-  return maxLength;
-}
+const originalObject = { name: "Alice", age: 30 };
+const clonedObject = JSON.parse(JSON.stringify(originalObject));
+console.log(clonedObject); // { name: "Alice", age: 30 }
